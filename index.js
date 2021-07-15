84a27ca0ea00a34e38dc16ae88c4cb9e0e34945a
Gregorio Lucena <gregoriolucena91@gmail.com>
2021-07-15 16:40:59 -0400

    four commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const hello = () => {
     console.log('tesintg....')
+    console.log('tesintg 4....')
     // console.log('Realizo pruebas en ADempiere desde la opcion de la intranet (ADempiere 393 Ambiente de Pruebas)')
 }
 
